a56ecd9a3ea00b1b6354a80f9524fb0121dd6754
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-04-26 17:10:24 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,118 @@
+//
+// Created by shaharnik on 26/04/2020.
+//
+//
+// Created by shaharnik on 26/04/2020.
+//
+#include "doctest.h"
+#include "solver.hpp"
+#include <string>
+
+using namespace std;
+using namespace solver;
+
+TEST_CASE("Tests") {
+
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+            CHECK(1==1);
+
+}
+
